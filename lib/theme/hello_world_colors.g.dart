3706d0be3dec16e6e697cdb6ce51ef4281c8b723
Parabeac-Nest <admin@parabeac.com>
2022-11-06 21:10:06 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/hello_world_colors.g.dart
@@ -0,0 +1,12 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class HelloWorldColors {
+
+static const Color lightSecondary = Color(0xff10a81f);
+}
