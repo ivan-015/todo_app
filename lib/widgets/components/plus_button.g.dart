4ac572f0a9f8bc83168e9bbc04ffa05b65e16928
Parabeac-Nest <admin@parabeac.com>
2022-11-20 23:28:45 +0000

    Auto-generated commit by Parabeac Cloud

--- lib/widgets/components/plus_button.g.dart
+++ lib/widgets/components/plus_button.g.dart
@@ -9,11 +9,10 @@ import 'package:todo_app/widgets/components/custom/plus_button_custom.dart';
 
 class PlusButton extends StatefulWidget {
   final BoxConstraints constraints;
-  final Widget? ovrPlusBackground;
+
   const PlusButton(
     this.constraints, {
     Key? key,
-    this.ovrPlusBackground,
   }) : super(key: key);
   @override
   _PlusButton createState() => _PlusButton();
@@ -25,66 +24,47 @@ class _PlusButton extends State<PlusButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(),
+        decoration: BoxDecoration(
+          color: Color(0xff404040),
+          borderRadius: BorderRadius.all(Radius.circular(30)),
+        ),
         child: PlusButtonCustom(
             child: Stack(children: [
           Positioned(
-            left: 0,
-            width: widget.constraints.maxWidth * 1.0,
-            top: 0,
-            height: widget.constraints.maxHeight * 1.0,
-            child: Stack(children: [
-              Positioned(
-                right: 0,
-                width: 60.0,
-                bottom: 0,
-                height: 60.0,
-                child: widget.ovrPlusBackground ??
-                    Image.asset(
-                      'assets/images/plusbackground.png',
-                      package: 'todo_app',
-                      height: 60.0,
-                      width: 60.0,
-                      fit: BoxFit.none,
+            left: 20.0,
+            width: 20.0,
+            top: 20.0,
+            height: 20.0,
+            child: Container(
+                decoration: BoxDecoration(),
+                child: Stack(children: [
+                  Positioned(
+                    left: 9.0,
+                    width: 2.0,
+                    top: 0,
+                    height: 20.0,
+                    child: Container(
+                      height: 20.0,
+                      width: 2.0,
+                      decoration: BoxDecoration(
+                        color: Color(0xffd9d9d9),
+                      ),
                     ),
-              ),
-              Positioned(
-                left: 20.0,
-                width: 20.0,
-                top: 20.0,
-                height: 20.0,
-                child: Container(
-                    decoration: BoxDecoration(),
-                    child: Stack(children: [
-                      Positioned(
-                        left: 9.0,
-                        width: 2.0,
-                        top: 0,
-                        height: 20.0,
-                        child: Container(
-                          height: 20.0,
-                          width: 2.0,
-                          decoration: BoxDecoration(
-                            color: Color(0xffd9d9d9),
-                          ),
-                        ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    width: 20.0,
+                    top: 9.0,
+                    height: 2.0,
+                    child: Container(
+                      height: 2.0,
+                      width: 20.0,
+                      decoration: BoxDecoration(
+                        color: Color(0xffd9d9d9),
                       ),
-                      Positioned(
-                        left: 0,
-                        width: 20.0,
-                        top: 9.0,
-                        height: 2.0,
-                        child: Container(
-                          height: 2.0,
-                          width: 20.0,
-                          decoration: BoxDecoration(
-                            color: Color(0xffd9d9d9),
-                          ),
-                        ),
-                      ),
-                    ])),
-              ),
-            ]),
+                    ),
+                  ),
+                ])),
           ),
         ])));
   }
--- lib/widgets/components/save_todo_button.g.dart
+++ lib/widgets/components/save_todo_button.g.dart
@@ -35,22 +35,23 @@ class _SaveTodoButton extends State<SaveTodoButton> {
           Positioned(
             left: 152.0,
             right: 153.0,
-            top: 13.0,
-            height: 23.0,
-            child: Container(
-                height: 23.0,
-                width: widget.constraints.maxWidth * 0.1527777777777778,
-                child: AutoSizeText(
-                  widget.ovrSave ?? 'Save',
-                  style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 0,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                )),
+            top: widget.constraints.maxHeight * 0.26,
+            height: widget.constraints.maxHeight * 0.46,
+            child: Center(
+                child: Container(
+                    height: 23.0,
+                    width: widget.constraints.maxWidth * 0.1527777777777778,
+                    child: AutoSizeText(
+                      widget.ovrSave ?? 'Save',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 0,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ))),
           ),
         ])));
   }
