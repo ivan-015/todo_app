85972b3d66f2ddc71b73a68f107e20bafe1e021d
Parabeac-Nest <admin@parabeac.com>
2022-11-20 23:51:09 +0000

    Auto-generated commit by Parabeac Cloud

--- lib/widgets/components/plus_button.g.dart
+++ lib/widgets/components/plus_button.g.dart
@@ -9,10 +9,11 @@ import 'package:todo_app/widgets/components/custom/plus_button_custom.dart';
 
 class PlusButton extends StatefulWidget {
   final BoxConstraints constraints;
-
+  final Widget? ovrPlusBackground;
   const PlusButton(
     this.constraints, {
     Key? key,
+    this.ovrPlusBackground,
   }) : super(key: key);
   @override
   _PlusButton createState() => _PlusButton();
@@ -24,47 +25,66 @@ class _PlusButton extends State<PlusButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: Color(0xff404040),
-          borderRadius: BorderRadius.all(Radius.circular(30)),
-        ),
+        decoration: BoxDecoration(),
         child: PlusButtonCustom(
             child: Stack(children: [
           Positioned(
-            left: 20.0,
-            width: 20.0,
-            top: 20.0,
-            height: 20.0,
-            child: Container(
-                decoration: BoxDecoration(),
-                child: Stack(children: [
-                  Positioned(
-                    left: 9.0,
-                    width: 2.0,
-                    top: 0,
-                    height: 20.0,
-                    child: Container(
-                      height: 20.0,
-                      width: 2.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xffd9d9d9),
-                      ),
+            left: 0,
+            width: widget.constraints.maxWidth * 1.0,
+            top: 0,
+            height: widget.constraints.maxHeight * 1.0,
+            child: Stack(children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 0,
+                bottom: 0,
+                child: widget.ovrPlusBackground ??
+                    Image.asset(
+                      'assets/images/plusbackground.png',
+                      package: 'todo_app',
+                      height: widget.constraints.maxHeight * 1.0,
+                      width: widget.constraints.maxWidth * 1.0,
+                      fit: BoxFit.contain,
                     ),
-                  ),
-                  Positioned(
-                    left: 0,
-                    width: 20.0,
-                    top: 9.0,
-                    height: 2.0,
-                    child: Container(
-                      height: 2.0,
-                      width: 20.0,
-                      decoration: BoxDecoration(
-                        color: Color(0xffd9d9d9),
+              ),
+              Positioned(
+                left: widget.constraints.maxWidth * 0.2,
+                width: widget.constraints.maxWidth * 0.6,
+                top: widget.constraints.maxHeight * 0.2,
+                height: widget.constraints.maxHeight * 0.6,
+                child: Container(
+                    decoration: BoxDecoration(),
+                    child: Stack(children: [
+                      Positioned(
+                        left: widget.constraints.maxWidth * 0.27,
+                        width: widget.constraints.maxWidth * 0.06,
+                        top: 0,
+                        height: widget.constraints.maxHeight * 0.6,
+                        child: Container(
+                          height: widget.constraints.maxHeight * 0.6,
+                          width: widget.constraints.maxWidth * 0.06,
+                          decoration: BoxDecoration(
+                            color: Color(0xffd9d9d9),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                ])),
+                      Positioned(
+                        left: 0,
+                        width: widget.constraints.maxWidth * 0.6,
+                        top: widget.constraints.maxHeight * 0.27,
+                        height: widget.constraints.maxHeight * 0.06,
+                        child: Container(
+                          height: widget.constraints.maxHeight * 0.06,
+                          width: widget.constraints.maxWidth * 0.6,
+                          decoration: BoxDecoration(
+                            color: Color(0xffd9d9d9),
+                          ),
+                        ),
+                      ),
+                    ])),
+              ),
+            ]),
           ),
         ])));
   }
